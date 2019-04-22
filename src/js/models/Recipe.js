4a09a253e5ef61299ddc562ffd4038320918f4e0
Ochanissi <mirelalex2094@gmail.com>
2019-04-22 12:24:01 +0300

    building the recipe model

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -31,4 +31,25 @@ export default class Recipe {
     calcServings() {
         this.servings = 4;
     }
+
+    psrseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounce', 'ounces', 'teaspoon', 'teaspoons', 'cups', 'pound'];
+        const unitShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1. Uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitShort[i]);
+            });
+
+            // 2. Remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+            // 3. Parse ingredients into count, unit and ingredient
+
+            return ingredient;
+
+        });
+        this.ingredients = newIngredients;
+    }
 }
