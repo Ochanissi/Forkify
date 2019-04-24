0588fa757f1bb1a37620c6850f58836823be20f2
Ochanissi <mirelalex2094@gmail.com>
2019-04-24 19:28:33 +0300

    building the recipe view - Highlight selected

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,16 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 };
 
+export const hightlightSelected = id => {
+    const resultsArr = Array.from(documen.querySelectorAll('.results__link'));
+
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
+
 /*
 // 'Pasta with tomato and spinach'
 acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
